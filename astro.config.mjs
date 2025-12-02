d72d620779aac1f8cc048821da479e38c5c17c89
Daniel Gómez <danigb2904@gmail.com>
2025-12-02 19:14:58 +0100

    final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercelAdapter from '@astrojs/vercel';
 
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,5 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  output: 'static',
+  adapter: vercelAdapter()
 });
